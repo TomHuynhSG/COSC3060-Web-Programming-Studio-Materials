f598f86c37e538ab234bd27c5fe17d6e7dcd83a4
Tom Huynh <tom.huynh@rmit.edu.vn>
2024-04-16 15:25:47 +0700

    add solutions

--- accessibility/accessibility-part-1/solutions/activity-3/script.js
+++ accessibility/accessibility-part-1/solutions/activity-3/script.js
@@ -5,18 +5,23 @@ function myValidate(event) {
     event.preventDefault(); // Prevent the form from submitting normally
 
     var nameInput = document.querySelector('#nameInput').value; // Get the value from the name input field
+    console.log(nameInput);
     var errorMessageDiv = document.querySelector('#errorMessage'); // Get the error message div
 
-    if (nameInput === '') {
-        errorMessageDiv.textContent = 'Please enter your name.'; // Set the text of the error message
+    if (nameInput == '') {
+        errorMessageDiv.textContent = 'Error: The name field is required. Please enter your name to proceed.'; // Set the text of the error message
         errorMessageDiv.style.display = 'block'; // Show the error message div
-        setTimeout(function() {
-            errorMessageDiv.style.display = 'none'; // Hide the error message div after 2 seconds
-            errorMessageDiv.textContent = ''; // Clear the error message
-        }, 2000);
+
+        // Focus the error message div so that screen readers will read it out.
+        errorMessageDiv.tabIndex = -1;
+        errorMessageDiv.focus();
+
     } else {
+        errorMessageDiv.textContent = '';
         errorMessageDiv.style.display = 'none';
-        alert('Submitted name: '+ nameInput); // Log the submitted name if the input is not empty
+        // Proceed with form submission or further actions
+        alert('Submitted name: ' + nameInput);
+        
         // Add your code here to submit the form or perform additional actions
     }
 }
