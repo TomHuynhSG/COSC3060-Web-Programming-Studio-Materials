ec19d86af9f2a442a4912ccbc14d9a05aa0e7c62
Tom Huynh <tom.huynh@rmit.edu.vn>
2024-05-19 22:44:27 +0700

    Update app.js

--- multer-file-upload/lecture/4.basic-website-file-upload-example-with-ejs/app.js
+++ multer-file-upload/lecture/4.basic-website-file-upload-example-with-ejs/app.js
@@ -14,24 +14,7 @@ const storage = multer.diskStorage({
 // Init upload
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 1000000 }, // Limit file size to 1MB
-  fileFilter: function(req, file, cb) {
-    checkFileType(file, cb);
-  }
 }).single('myImage'); // Name of the file input field
-
-// Check file type
-function checkFileType(file, cb) {
-  const filetypes = /jpeg|jpg|png|gif/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb('Error: Images Only!');
-  }
-}
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
