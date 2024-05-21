82c7589eb2a9af1f3a1a156e8abc835c923c4385
Tom Huynh <tom.huynh@rmit.edu.vn>
2024-05-21 11:29:29 +0700

    add more user data

--- google-oauth-authentication/lecture/simple-demo/config/passport-setup.js
+++ google-oauth-authentication/lecture/simple-demo/config/passport-setup.js
@@ -1,22 +1,27 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-require('dotenv').config();
-
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
-  },
-  (accessToken, refreshToken, profile, done) => {
-    // Here, you can save the profile information to your database if needed
-    return done(null, profile);
-  }
-));
 
 passport.serializeUser((user, done) => {
   done(null, user);
 });
 
-passport.deserializeUser((user, done) => {
-  done(null, user);
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
 });
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: '/auth/google/callback'
+},
+(accessToken, refreshToken, profile, done) => {
+  // Extract relevant user data from the profile object
+  const user = {
+    id: profile.id,
+    displayName: profile.displayName,
+    emails: profile.emails,
+    photos: profile.photos
+  };
+  done(null, user);
+}
+));
